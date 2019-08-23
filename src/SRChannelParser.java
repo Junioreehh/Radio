5204193954bca6b39cf725d074c5bd35bd671214
Junioreehh <54080920+Junioreehh@users.noreply.github.com>
2019-08-24 00:20:49 +0200

    Kodkonvention

--- src/SRChannelParser.java
+++ src/SRChannelParser.java
@@ -11,7 +11,7 @@ import java.net.URL;
 import java.util.ArrayList;
 
 public class SRChannelParser {
-    private Document XMLDocument;
+    private Document xmlDocument;
 
     /**
      * Constructs a SRChannelParser
@@ -22,17 +22,19 @@ public class SRChannelParser {
 
     /**
      * Returns the names of the "channel" nodes attribute "name" of the current
-     * XMLDocument
+     * xmlDocument
      * @return An ArrayList with strings
      */
     public ArrayList<String> getChannels() {
         ArrayList<String> channelNames = new ArrayList<>();
-        if(XMLDocument == null){
+        
+        if (xmlDocument == null) {
             channelNames.add("Error occurred retrieving channels");
             return channelNames;
         }
-        NodeList channelsNodes = XMLDocument.getElementsByTagName("channel");
-        for(int i = 0; i < channelsNodes.getLength();i++){
+        
+        NodeList channelsNodes = xmlDocument.getElementsByTagName("channel");
+        for (int i = 0; i < channelsNodes.getLength(); i++) {
             channelNames.add(channelsNodes.item(i).getAttributes()
                             .getNamedItem("name").getNodeValue());
         }
@@ -47,20 +49,21 @@ public class SRChannelParser {
     public String getChannelDesc(String channelName) {
         Node currentNode = getChannelsNode();
         NodeList channelNodeList = currentNode.getChildNodes();
-        if(XMLDocument == null){
+        
+        if (xmlDocument == null) {
             return "Couldn't retrieve description";
         }
 
         /*Finds the description for the corresponding channel name*/
-        for(int i = 0;i < channelNodeList.getLength();i++){
+        for (int i = 0; i < channelNodeList.getLength(); i++) {
             currentNode = channelNodeList.item(i);
-            if(currentNode.getNodeType() == 1){
-                if(currentNode.getAttributes().getNamedItem("name")
-                        .getNodeValue().equals(channelName)){
-                    for(int j = 0; j < currentNode.getChildNodes().getLength()
-                            ;j++){
-                        if(currentNode.getChildNodes().item(j).getNodeName()
-                                      .equals("tagline")){
+            if (currentNode.getNodeType() == 1) {
+                if (currentNode.getAttributes().getNamedItem("name")
+                        .getNodeValue().equals(channelName)) {
+                    for (int j = 0; j < currentNode.getChildNodes().getLength()
+                            ; j++) {
+                        if (currentNode.getChildNodes().item(j).getNodeName()
+                                      .equals("tagline")) {
                             return currentNode.getChildNodes().item(j).
                                     getTextContent();
                         }
@@ -79,17 +82,18 @@ public class SRChannelParser {
     public String getChannelImageURL(String channelName) {
         Node currentNode = getChannelsNode();
         NodeList channelNodeList = currentNode.getChildNodes();
-        if(XMLDocument == null){
+        
+        if (xmlDocument == null) {
             return "https://static-cdn.sr.se/sida/images/3113/2216702_" +
                     "512_512.jpg?preset=socialmedia-share-image";
         }
 
         /*Finds the image URL for the corresponding channel name*/
-        for(int i = 0;i < channelNodeList.getLength();i++){
+        for (int i = 0; i < channelNodeList.getLength(); i++) {
             currentNode = channelNodeList.item(i);
-            if(currentNode.getNodeType() == 1){
-                if(currentNode.getAttributes().getNamedItem("name")
-                              .getNodeValue().equals(channelName)){
+            if (currentNode.getNodeType() == 1) {
+                if (currentNode.getAttributes().getNamedItem("name")
+                              .getNodeValue().equals(channelName)) {
                     return currentNode.getChildNodes().item(1)
                                       .getTextContent();
                 }
@@ -109,11 +113,11 @@ public class SRChannelParser {
         NodeList channelNodeList = currentNode.getChildNodes();
 
         /*Finds the image URL for the corresponding channel name*/
-        for(int i = 0;i < channelNodeList.getLength();i++){
+        for (int i = 0; i < channelNodeList.getLength(); i++) {
             currentNode = channelNodeList.item(i);
-            if(currentNode.getNodeType() == 1){
-                if(currentNode.getAttributes().getNamedItem("name")
-                              .getNodeValue().equals(channelName)){
+            if (currentNode.getNodeType() == 1) {
+                if (currentNode.getAttributes().getNamedItem("name")
+                              .getNodeValue().equals(channelName)) {
                     return currentNode.getAttributes().getNamedItem("id")
                                       .getNodeValue();
                 }
@@ -123,25 +127,24 @@ public class SRChannelParser {
     }
 
     /**
-     * Updates the XMLDocument containing the channels
+     * Updates the xmlDocument containing the channels
      */
-    public void updateDocument() throws IOException{
+    public void updateDocument() throws IOException {
         DocumentBuilderFactory documentBuilderFactory
                 = DocumentBuilderFactory.newInstance();
 
         try {
             DocumentBuilder db = documentBuilderFactory.newDocumentBuilder();
-            XMLDocument = db.parse(new URL("http://api.sr.se/api/v2/" +
+            xmlDocument = db.parse(new URL("http://api.sr.se/api/v2/" +
                     "channels?pagination=false")
                     .openStream());
-        }catch (ParserConfigurationException e){
+        }catch (ParserConfigurationException e) {
             System.err.println("Unable to configure parser");
             JFrame errorMessage = new JFrame();
             errorMessage.add(new JTextArea("Någonting gick snett,prova igen"));
-        }catch (SAXException e){
+        }catch (SAXException e) {
             System.err.println("Not correct format");
             System.exit(0);
-
         }
     }
 
@@ -153,10 +156,10 @@ public class SRChannelParser {
         Node currentNode = null;
 
         /*Finds the "channels" node in the document*/
-        NodeList channelNodeList = XMLDocument.getFirstChild().getChildNodes();
-        for(int i = 0;i < channelNodeList.getLength();i++){
+        NodeList channelNodeList = xmlDocument.getFirstChild().getChildNodes();
+        for (int i = 0; i < channelNodeList.getLength(); i++) {
             currentNode = channelNodeList.item(i);
-            if(currentNode.getNodeName().equals("channels")){
+            if (currentNode.getNodeName().equals("channels")) {
                 return currentNode;
             }
         }
